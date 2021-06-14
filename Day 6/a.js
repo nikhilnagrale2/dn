cefca6263637e50ff1b47c8eeb234283b8c8f8e1
Nikhil Nagrale <nknagrale634@gmail.com>
2021-06-14 21:44:17 +0530

    Day 6

---
+++ Day 6/a.js
@@ -0,0 +1,7 @@
+function is_array(params) {
+    if(typeof(params)=="object") return true;
+    else return false;
+}
+
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
---
+++ Day 6/d.js
@@ -0,0 +1,6 @@
+function convert_string(color) {
+    return color.toString();
+}
+
+myColor = ["Red", "Green", "White", "Black"];
+console.log(convert_string(myColor));
