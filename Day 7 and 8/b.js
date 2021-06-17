b5bf15b193c74df1163c6384a123767b351aa5c2
Nikhil Nagrale <nknagrale634@gmail.com>
2021-06-17 16:57:05 +0530

    Day 7 and 8

---
+++ Day 7 and 8/b.js
@@ -0,0 +1,8 @@
+var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+console.log("Before Delete");
+console.log(student);
+
+delete student.rollno;
+
+console.log("After Delete");
+console.log(student);
---
+++ Day 7 and 8/d.js
@@ -0,0 +1,16 @@
+var library = [
+  { author: "Bill Gates", title: "The Road Ahead", readingStatus: true },
+  { author: "Steve Jobs", title: "Walter Isaacson", readingStatus: true },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+for (let item of library) {
+  console.log("Book Title: " + item.title);
+  console.log("Author Name: " + item.author);
+  console.log("Is Book Read? : " + item.readingStatus);
+  console.log();
+}
