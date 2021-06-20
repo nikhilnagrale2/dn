068184945b378f99e6260e5cb89bbd2784b4ea4c
Nikhil Nagrale <nknagrale634@gmail.com>
2021-06-20 19:45:21 +0530

    Day 5

--- Day 5/script.js
+++ Day 5/script.js
@@ -1,12 +1,29 @@
 window.onload = () => {
   const first_input = document.getElementById("first");
   const second_input = document.getElementById("second");
+  const buttons = document.getElementById("button-group");
+  const result = document.getElementById("result");
 
-  const buttons = document.querySelectorAll("button");
-
-  const add = buttons[0];
-  const subtract = buttons[1];
-  const multiply = buttons[2];
-  const divide = buttons[3];
-  console.log(first_input.value);
+  buttons.addEventListener("click", (e) => {
+    const firstValue = parseInt(first_input.value);
+    const secondValue = parseInt(second_input.value);
+    const operation = e.target.innerText;
+    switch (operation) {
+      case "Add":
+        result.innerText = "The result is " + (firstValue + secondValue);
+        break;
+      case "Subtract":
+        result.innerText = "The result is " + (firstValue - secondValue);
+        break;
+      case "Multiply":
+        result.innerText = "The result is " + firstValue * secondValue;
+        break;
+      case "Divide":
+        result.innerText = "The result is " + firstValue / secondValue;
+        break;
+      default:
+        alert("Wrong Operand");
+        break;
+    }
+  });
 };
