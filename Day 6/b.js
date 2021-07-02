a9b742aeac651b7c366b1f0857da2c129edbef5d
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-02 13:15:24 +0530

    deleted unnecessary trash

--- Day 6/b.js
+++
@@ -1,7 +0,0 @@
-function array_Clone(params) {
-  copyarray = [...params];
-  return copyarray;
-}
-
-console.log(array_Clone([1, 2, 4, 0]));
-console.log(array_Clone([1, 2, [4, 0]]));
--- Day-06/d.js
+++
@@ -1,6 +0,0 @@
-function convert_string(color) {
-  return color.toString();
-}
-
-myColor = ["Red", "Green", "White", "Black"];
-console.log(convert_string(myColor));
