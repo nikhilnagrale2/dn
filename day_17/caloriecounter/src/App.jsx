419f2b72cf07b47bdb838549d6f4f7ec425b18ae
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-06 02:43:54 +0530

    added functional components

---
+++ day_17/caloriecounter/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <h1 className="food">{props.food}</h1>
+      <h2 className="desc">You have consumed {props.calorie} cals today</h2>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calories Counter</h1>
+      <div className="container">
+        <Card food="Pizza" calorie="56" />
+        <Card food="Burger" calorie="69" />
+        <Card food="Coke" calorie="500" />
+        <Card food="Browne" calorie="190" />
+        <Card food="Fried Rice" calorie="90" />
+        <Card food="Lassania" calorie="200" />
+        <Card food="Pani Puri" calorie="10" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
