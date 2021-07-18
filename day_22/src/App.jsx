f909381ff3623c83e679d2110491e3cebf4bea8b
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-19 01:49:26 +0530

    app jx updated

--- day_22/src/App.jsx
+++ day_22/src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import Template from "./components/Templates";
+import Meme from "./components/Meme";
 
 function App() {
   const [templates, setTemplates] = useState([]);
+  const [meme, setMeme] = useState(null);
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
@@ -13,6 +16,11 @@ function App() {
   return (
     <div className="App">
       <h1 className="">Meme Generator</h1>
+      {meme === null ? (
+        <Template templates={templates} setMeme={setMeme} />
+      ) : (
+        <Meme meme={meme} setMeme={setMeme} />
+      )}
     </div>
   );
 }
