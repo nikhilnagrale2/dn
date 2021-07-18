dbc0da04e0edd70bdfaa0e665b04d40a0102c463
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-18 21:12:36 +0530

    functionality added

--- day_21/calorie_counter/src/App.jsx
+++ day_21/calorie_counter/src/App.jsx
@@ -1,46 +1,115 @@
 import "./App.css";
 import React, { useState } from "react";
 
-const foods = [
-  ["pizza", 56],
-  ["Burger", 69],
-  ["Coke", 500],
-  ["Browne", 190],
-  ["Fried Rice", 90],
-  ["Lassania", 200],
-  ["Pani Puri", 10],
-];
+const Card = ({ calsT, food, id, itemName, setFoods }) => {
+  const [item, setItem] = useState(itemName);
+  const [cals, setCals] = useState(calsT);
+  const [isEditMode, setEditMode] = useState(false);
+  // console.log(food);
+  return (
+    <div className="card">
+      <div className="container">
+        {isEditMode ? (
+          <>
+            <input
+              type="text"
+              value={item}
+              onChange={(e) => {
+                setItem(e.target.value);
+              }}
+            />
+            <input
+              value={cals}
+              type="number"
+              onChange={(e) => {
+                setCals(e.target.value);
+              }}
+            ></input>
+          </>
+        ) : (
+          <>
+            <h2 className="food">{item}</h2>
+            <h3 className="desc">You have consumed {cals} cals today</h3>
+            {cals > 180 ? (
+              <h3>
+                It Contains too much calories. Please don't eat much else you
+                will swell like ballon.
+              </h3>
+            ) : (
+              <h3>
+                It doesn't have much calories you can eat as much as you want.
+              </h3>
+            )}
+          </>
+        )}
+      </div>
+      <div className="container">
+        <button
+          className="delete"
+          onClick={() => {
+            console.log(food);
+            console.log(id);
+            const filteredCard = food.filter((e, ind) => {
+              console.log(ind);
+              return ind !== id;
+            });
+            setFoods(filteredCard);
+            console.log(filteredCard);
+          }}
+        >
+          Delete
+        </button>
+        {isEditMode ? (
+          <button
+            className="edit"
+            onClick={() => {
+              // console.log(e);
+              let newFoodList = food.map((i, ii) => {
+                // console.log(i, ii, id);
+                // console.log(item, cals);
+                if (ii === id) {
+                  return [item, cals];
+                } else {
+                  return i;
+                }
+              });
+              setFoods(newFoodList);
+              setEditMode(false);
+            }}
+          >
+            Save
+          </button>
+        ) : (
+          <button
+            className="edit"
+            onClick={() => {
+              setEditMode(true);
+            }}
+          >
+            Edit
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
-const Cards = () => {
-  const [food, setFood] = useState(foods);
+const Cards = ({ food, setFoods }) => {
+  // console.log(food);
   return (
     <div className="cards">
       {food.length !== 0 ? (
         food.map((item, index) => {
+          // console.log(item);
           return (
-            <div className="card">
-              <h1 className="food">{item[0]}</h1>
-              <h2 className="desc">You have consumed {item[1]} cals today</h2>
-              <button
-                className="delete"
-                onClick={() => {
-                  const filteredCard = food.filter((e, i) => i !== index);
-                  setFood(filteredCard);
-                }}
-              >
-                Delete
-              </button>
-              {item[1] > 180 ? (
-                <h3>
-                  It Contains too much calories. Please don't eat much else you
-                  will swell like ballon.
-                </h3>
-              ) : (
-                <h3>
-                  It doesn't have much calories you can eat as much as you want.
-                </h3>
-              )}
-            </div>
+            <Card
+              setFoods={setFoods}
+              key={index}
+              id={index}
+              food={food}
+              itemName={item[0]}
+              calsT={item[1]}
+            />
           );
         })
       ) : (
@@ -65,11 +134,50 @@ const Cards = () => {
 };
 
 function App() {
+  const [food, setFoods] = useState([]);
+  const [item, setItem] = useState("");
+  const [cal, setCal] = useState(0);
   return (
     <div className="App">
       <h1>Calories Counter</h1>
       <div className="container">
-        <Cards />
+        <form
+          className="addItem"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (item !== " ") {
+              setFoods(() => {
+                return [...food, [item, cal]];
+              });
+              setCal(0);
+              setItem("");
+            }
+          }}
+        >
+          <input
+            type="text"
+            required
+            placeholder="Enter food item name"
+            value={item}
+            onChange={(e) => {
+              setItem(e.target.value);
+            }}
+          />
+          <input
+            type="number"
+            placeholder="Enter calories"
+            required
+            value={cal}
+            onChange={(e) => {
+              setCal(e.target.value);
+            }}
+          />
+          <button type="submit">Add Item</button>
+        </form>
+      </div>
+
+      <div className="container">
+        <Cards food={food} setFoods={setFoods} />
       </div>
     </div>
   );
