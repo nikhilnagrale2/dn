be2e67472834c34b574b1ee438f10d5043795b5c
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-28 00:39:15 +0530

    About page

---
+++ day_24/src/router/About.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import Context from "../context/AuthContext";
+
+function About() {
+  const logger = useContext(Context);
+  return (
+    <div>
+      <h1>About</h1>
+      <div>{logger.loggedIn ? "You are logged in" : "You are logged Out"}</div>
+    </div>
+  );
+}
+
+export default About;
