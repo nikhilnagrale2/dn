deb94c78c31e27bc6d5a3c05174d2ef2cbeec794
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-31 06:04:00 +0530

    all core logic

--- day_23/src/App.js
+++ day_23/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useFetch from "./Hooks/useFetch";
 
 function App() {
+  const [quote] = useFetch("https://animechan.vercel.app/api/random");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="Container">
+        <h1>{quote.quote}</h1>
+        <h2>{quote.character}</h2>
+        <h3>{quote.anime}</h3>
+        <button
+          className="btn"
+          onClick={() => {
+            document.location.reload();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
